4a47c6f8dbcd0c03a557b10fd4497be7a834e6d7
jacobth <jacob.thureson@gmail.com>
2017-11-21 05:24:49 -0800

    added lab1 and 2

---
+++ lab2/TypeChecker.java
@@ -0,0 +1,15 @@
+import CPP.Absyn.*;
+
+public class TypeChecker {
+
+
+	public HashMap<String, FunType> signature;
+	public LinkedList<HashMap<String, Type>> contexts;
+
+	public void typecheck(Program p) {
+        	throw new TypeException("Not yet a typechecker");
+    	}
+
+	
+
+}
